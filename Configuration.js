cddca293563c8f2e8cc77213e6d2519766897277
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-04-17 14:12:28 -0300

    Initial commit
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

---
+++ Configuration.js
@@ -0,0 +1,89 @@
+function getConfiguration(config)
+{
+  // This function allows you to indicate general configuration values
+  // for all devices of this model.
+
+  // Depending on the meaning of the "device address" in this device, 
+  // you may want to change the label that is used to refer to the 
+  // address in the user interface.
+  // For instance, if the address of the device is actually a MAC 
+  // address, you may want to use the code below.
+  
+  config.addressLabel = {en: "DEV EUI", es: "DEV EUI"};
+}
+
+function getEndpoints(deviceAddress, endpoints)
+{
+  // This function allows you to indicate the initial endpoint configuration
+  // when a device is created using this model. This improves end-user 
+  // experience significantly, because it allows the platform to create
+  // all endpoints included in the device automatically when the device
+  // is created.
+
+  // In the code below, two endpoints are created. The first is a
+  // temperature sensor, while the second one is a carbon dioxide sensor.
+
+    var light = endpoints.addEndpoint("1", "Light Intensity", endpointType.lightSensor);
+
+    var watermark1_frequency = endpoints.addEndpoint("2", "Watermark 1 Frequency", endpointType.genericSensor);
+    watermark1_frequency.variableTypeId = 1362;
+
+    var watermark1_tension = endpoints.addEndpoint("3", "Watermark 1 Tension", endpointType.genericSensor);
+    watermark1_tension.variableTypeId = 1366;
+
+    var watermark2_frequency = endpoints.addEndpoint("4", "Watermark 2 Frequency", endpointType.genericSensor);
+    watermark2_frequency.variableTypeId = 1362;
+
+    var watermark2_tension = endpoints.addEndpoint("5", "Watermark 2 Tension", endpointType.genericSensor);
+    watermark2_tension.variableTypeId = 1366;
+
+
+  // endpoints.addEndpoint("2", "CO2 sensor", endpointType.ppmConcentrationSensor, ppmConcentrationSensorSubType.carbonDioxide);
+}
+
+function validateDeviceAddress(address, result)
+{
+  // This function allows you to validate the address of a device, when
+  // the user is creating it. If your device has special validation rules
+  // for the address, you can check them here, and return an error message
+  // in case the address format is incorrect.
+
+  // In the code below, a validation is made to ensure that the address 
+  // is exactly 10 characters long.
+  
+  // if (address.length != 10) {
+  //   result.ok = false;
+  //   result.errorMessage = {
+  //     en: "The address must be 10 characters long", 
+  //     es: "La dirección debe tener exactamente 10 caracteres"
+  //   };
+  // }
+}
+
+function updateDeviceUIRules(device, rules)
+{
+  // This function allows you to specify UI rules at the device level.
+  // For instance, you can make it possible to enable or disable adding
+  // endpoints manually to the device after it was created.
+  
+  // In the code below, adding endpoints manually is disabled in the
+  // user interface. This means that the device will be limited to the 
+  // endpoints defined by function getEndpoints() above.
+  
+  // rules.canCreateEndpoints = false;
+}
+
+function updateEndpointUIRules(endpoint, rules)
+{
+  // This function allows you to specify UI rules at the endpoint level.
+  // For instance, you can make it possible to delete certain endpoints, 
+  // or edit their endpoint subtype, if applicable.
+
+  // In the code below, the following rules are defined:
+  // - Endpoints cannot be deleted.
+  // - The endpoint subtype can be changed, but only for the second 
+  //   endpoint.
+  
+  // rules.canDelete = false;
+  // rules.canEditSubType = (endpoint.address == "2");
+}
